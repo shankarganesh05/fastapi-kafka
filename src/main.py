75ad7b182b3415a09958fcc642d67df79f2bc164
shankarganesh05 <shankarganesh.d@gmail.com>
2025-07-14 19:57:14 +0530

    Added Get individual order

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status,HTTPException
 from typing import List
 from src.model import Order,getOrder
 from src.kafka_producer import send_order_kafka
@@ -8,15 +8,16 @@ from src.db import engine,get_session,Orders
 import threading
 
 app = FastAPI()
-SQLModel.metadata.create_all(engine)
+#SQLModel.metadata.create_all(engine)
 
 @app.on_event("startup")
 def startup_event():
+    SQLModel.metadata.create_all(engine)
     t= threading.Thread(target=consume_order,daemon=True)
     t.start()
 
 
-@app.post("/orders/")
+@app.post("/orders/",status_code = status.HTTP_201_CREATED)
 async def create_order(order: Order):
     order_data = order.dict()
     send_order_kafka(order_data)
@@ -28,4 +29,11 @@ def get_order(db: Session= Depends(get_session)):
     print(orders)
     return orders
 
+@app.get("/orders/{order_id}",response_model=getOrder)
+def get_order(order_id:int,db: Session= Depends(get_session)):
+    order = db.get(Orders,order_id)
+    if order:
+        return order
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Order with id {order_id} does not exist")
+    
 
